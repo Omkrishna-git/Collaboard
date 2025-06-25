41ebdf5b4c7a3fbb7b2f2886acf8836257df9aae
Omkrishna-git <omdpatil07@gmail.com>
2025-06-25 11:31:53 +0530

    setup and authentication done

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://sweeping-panda-71.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ],
+};
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // any other options...
+}
+
+module.exports = nextConfig

70d7e491bf30ebe2c87389ad8fc01d4bed38ef99
Omkrishna-git <omdpatil07@gmail.com>
2025-06-27 22:14:31 +0530

    canvas layout :- 1.info 2.participants 3.toolbar

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Canvas } from "./_components/canvas";
+// import { Room } from "@/components/room";
+// import { Loading } from "./_components/loading";
+import { useEffect } from "react";
+
+interface BoardIdPageProps {
+    params: { 
+        boardId: string 
+    };
+}
+
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+    useEffect(() => {
+        document.title = `Collaboard`;
+    }, []);
+
+    return (
+        // <Room roomId={params.boardId} fallback={<Loading />}>
+            <Canvas boardId={params.boardId} />
+        // </Room>
+    );
+};
+
+export default BoardIdPage;
